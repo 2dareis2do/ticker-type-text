931fd0edab34466bc08fbe11bf5eb2add38e9aca
Daniel Lobo <info@danlobo.co.uk>
2021-03-22 16:13:04 +0000

    - working well with multiple items - ensure cleanup as events do not alwats fire in time

--- index.js
+++ index.js
@@ -7,83 +7,88 @@
   $.fn.writeText = function (contents, keep, seconds, delay = 10) {
     var current = 0,
       count = 0,
+      deviance = .99;
       elem = this;
 
-      console.log("count init", count);
-      console.log("contents.length", contents.length);
-
-    // here we have the initial animation
+    // here we have the initial animation - on load
     setTimeout(function () {
+        var contentArray = contents[0].textContent.split("");
 
-      var contentArray = contents[0].textContent.split("");
-      var add = setInterval(function () {
-        if (current < contentArray.length) {
-          elem.text(elem.text() + contentArray[current++]);
-        }
-      }, delay);
+        let maxInDelay = Math.floor((seconds -1) * 1000 * deviance / contents[0].textContent.length);
+        var add = setInterval(function () {
+            if (current < contentArray.length) {
+            elem.text(elem.text() + contentArray[current++]);
+            }
+        }, (delay <= maxInDelay ? delay : maxInDelay));
 
+      // remove or subtract
       setTimeout(function () {
+
+        let maxOutDelay = Math.floor(1000 * deviance / elem.text().length);
+
         clearInterval(add);
         count++;
-        console.log("count", count);
 
         var subtract = setInterval(function () {
           if (current > keep) {
             elem.text(elem.text().substring(0, elem.text().length - 1));
             current--;
           }
-        }, 10);
+        }, (delay <= maxOutDelay ? delay : maxOutDelay));
 
-        setTimeout(function () {
-            clearInterval(subtract);
-        }, 985);
+          setTimeout(function () {
+              //make sure we clear as does not always complete
+              if (elem.text()) {
+                  elem.text("");
+              }
+              clearInterval(subtract);
+          }, (1000 * deviance));
 
       }, 4000);
 
-    //   setTimeout(function () {
-    //     // count++;
-    //     // console.log("count", count);
-    //   }, 4995);
-
     }, 0);
 
-    // this fires of after the time limit (default 5000 ms)
+    // here we have the recurring animation
     setInterval(function () {
-
         var current = 0;
+
+        let maxInDelay = Math.floor(((seconds - 1) * 1000 * deviance ) / (contents[(count) % contents.length].textContent.length));
+        // console.log("maxInDelay 2", maxInDelay);
 
         var contentArray = contents[(count) % contents.length].textContent.split("");
 
-      var add2 = setInterval(function () {
+        var add = setInterval(function () {
 
         if (current < contentArray.length) {
           elem.text(elem.text() + contentArray[current++]);
         }
 
+        }, (delay <= maxInDelay ? delay : maxInDelay));
 
-      }, delay);
+     // remove or subtract
+
 
       setTimeout(function () {
 
-        clearInterval(add2);
+        let maxOutDelay = Math.floor(1000 * deviance / elem.text().length);
+        clearInterval(add);
         count++;
-        console.log("count2", count);
 
-        var subtract2 = setInterval(function () {
+        var subtract = setInterval(function () {
           if (current > keep) {
             elem.text(elem.text().substring(0, elem.text().length - 1));
             current--;
           }
-        }, delay);
+        }, (delay <= maxOutDelay ? delay : maxOutDelay ));
 
           setTimeout(function () {
-              clearInterval(subtract2);
-          }, 985);
+              if (elem.text()) {
+                  elem.text("");
+              }
+              clearInterval(subtract);
+          }, (1000 * deviance));
 
       }, 4000);
-
-
-
 
     }, seconds * 1000);
 
@@ -93,7 +98,7 @@
 
 let animatetext = function(){
   let $elements = $(".tt-holder .tt");
-  $(".ttt").writeText($elements, 0, 5, 20);
+  $(".ttt").writeText($elements, 0, 5, 40);
 }
 
 $(animatetext) ;
