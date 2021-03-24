7696d559fe1ceb61388472c4a4abc806cc49294a
Daniel Lobo <info@danlobo.co.uk>
2021-03-24 00:39:17 +0000

    - working - fixed issue where it was always using first value

--- index.js
+++ index.js
@@ -5,66 +5,51 @@
 ** seconds => integer of seconds it takes to iterate through slide
 ** speed => integer of delay denoting milliseconds default = 20
 */
-  $.fn.writeText = function (contents, keep, seconds, delay = 10) {
+  $.fn.writeText = function (contents, keep, seconds, delay = 20) {
     var current = 0,
       count = 0,
-      deviance = .90,
-      inT = .85,
-      ouT = .15,
+      deviance = 1,
+      inTransPercent = .80,
+      outTransPercent = .20,
       elem = this;
 
-    // here we have the initial animation - on load
+    // here we have the initial animation that starts at 0 seconds - part 1
     setTimeout(function () {
         var contentArray = contents[0].textContent.split("");
 
-        let maxInDelay = Math.floor((seconds -1) * 1000 * deviance / contents[0].textContent.length);
+        let maxInDelay = Math.floor((seconds * inTransPercent) * 1000 / contents[0].textContent.length);
         let timeoutAddCount = 0;
+
+        // add text chars - recursive function that runs like setInterval using setTimeuut for consitentcy and greater control
         function timeoutAdd() {
             setTimeout(function () {
-                     //do stuff
+            //do stuff
                 if (current < contentArray.length) {
                     elem.text(elem.text() + contentArray[current++]);
                 }
-                // console.log("timeoutCount", timeoutCount);
-                // console.log("contents[0].textContent.length", contents[0].textContent.length);
-
 
                 if (timeoutAddCount >= contents[0].textContent.length) {
                     return;
                 }
                 timeoutAddCount++;
 
-                    timeoutAdd();
+                timeoutAdd();
 
             }, (delay <= maxInDelay ? delay : maxInDelay));
         }
 
         timeoutAdd();
 
-        // var add = setInterval(function () {
-        //     if (current < contentArray.length) {
-        //         elem.text(elem.text() + contentArray[current++]);
-        //     }
-        // }, (delay <= maxInDelay ? delay : maxInDelay));
+        // remove text chars - delayed to start at outTransPercent inTransPercent
+        setTimeout(function () {
 
-      // remove or subtract
-      setTimeout(function () {
+        let maxOutDelay = Math.floor((seconds * outTransPercent) * 1000 / elem.text().length);
 
-        let maxOutDelay = Math.floor(1000 * deviance / elem.text().length);
-
-        // clearInterval(add);
         count++;
 
-        // var subtract = setInterval(function () {
-        //   if (current > keep) {
-        //     elem.text(elem.text().substring(0, elem.text().length - 1));
-        //     current--;
-        //   }
-        // }, (delay <= maxOutDelay ? delay : maxOutDelay));
-
-          let timeoutSubtractCount = 0;
-          function timeoutSubract() {
-              setTimeout(function () {
+        let timeoutSubtractCount = 0;
+        function timeoutSubract() {
+            setTimeout(function () {
                   //do stuff
                   if (current > keep) {
                       elem.text(elem.text().substring(0, elem.text().length - 1));
@@ -88,12 +73,13 @@
               if (!keep && elem.text()) {
                   elem.text("");
               }
-            //   clearInterval(subtract);
-          }, Math.floor(1000 * ouT * seconds * deviance));
+          }, Math.floor((1000 * outTransPercent * seconds) - deviance));
 
-      }, Math.floor(1000 * inT * seconds) );
+      }, Math.floor(1000 * inTransPercent * seconds) );
 
     }, 0);
+
+    // part starts after x seconds parameter
 
     function timeout() {
         setTimeout(function () {
@@ -102,40 +88,23 @@
             // create a recursive loop.
             var current = 0;
 
-            let maxInDelay = Math.floor(((seconds - 1) * 1000 * deviance) / (contents[(count) % contents.length].textContent.length));
+            let maxInDelay = Math.floor(((seconds * inTransPercent) * 1000) / (contents[count % contents.length].textContent.length));
 
-            var contentArray = contents[(count) % contents.length].textContent.split("");
+            var contentArray = contents[count % contents.length].textContent.split("");
 
-            // var add = setInterval(function () {
-
-            //     if (current < contentArray.length && !keep) {
-
-            //         elem.text(elem.text() + contentArray[current++]);
-            //     } else {
-            //         if (keep + current < contentArray.length) {
-            //             elem.text(elem.text() + contentArray[keep + current++]);
-            //         }
-            //     }
-
-            // }, (delay <= maxInDelay ? delay : maxInDelay));
 
             let timeoutAddCount2 = 0;
             function timeoutAdd2() {
                 setTimeout(function () {
                     //do stuff
                     if (current < contentArray.length && !keep) {
-
                         elem.text(elem.text() + contentArray[current++]);
-                    } else {
-                        if (keep + current < contentArray.length) {
-                            elem.text(elem.text() + contentArray[keep + current++]);
-                        }
+                    } else if (keep + current < contentArray.length) {
+                        elem.text(elem.text() + contentArray[keep + current++]);
                     }
-                    // console.log("timeoutCount", timeoutCount);
-                    // console.log("contents[0].textContent.length", contents[0].textContent.length);
 
 
-                    if (timeoutAddCount2 >= contents[0].textContent.length) {
+                    if (timeoutAddCount2 >= contents[count % contents.length].textContent.length) {
                         return;
                     }
                     timeoutAddCount2++;
@@ -147,40 +116,17 @@
 
             timeoutAdd2();
 
-
             // remove or subtract
-
-
             setTimeout(function () {
 
-                let maxOutDelay = Math.floor(1000 * deviance / elem.text().length);
-                // clearInterval(add);
+                let maxOutDelay = Math.floor(1000 * (seconds * outTransPercent) / elem.text().length);
                 count++;
-
-                // var subtract = setInterval(function () {
-                //     if (current > keep) {
-
-                //         elem.text(elem.text().substring(0, elem.text().length - 1));
-                //         current--;
-                //     } else {
-                //         current = elem.text().length--;
-                //         if (current > keep) {
-
-                //             elem.text(elem.text().substring(0, elem.text().length - 1));
-                //             current--;
-                //         }
-                //     }
-
-                // }, (delay <= maxOutDelay ? delay : maxOutDelay));
 
                 let timeoutSubtractCount2 = 0;
                 function timeoutSubract2() {
                     setTimeout(function () {
                         //do stuff
-                        // if (current > keep) {
-                        //     elem.text(elem.text().substring(0, elem.text().length - 1));
-                        //     current--;
-                        // }
+
                         if (current > keep) {
 
                             elem.text(elem.text().substring(0, elem.text().length - 1));
@@ -194,7 +140,7 @@
                             }
                         }
 
-                        if (timeoutSubtractCount2 >= contents[0].textContent.length) {
+                        if (timeoutSubtractCount2 >= contents[count % contents.length].textContent.length) {
                             return;
                         }
                         timeoutSubtractCount2++;
@@ -211,15 +157,13 @@
 
                         elem.text("");
                     }
-                    // clearInterval(subtract);
-                }, Math.floor(1000 * ouT * seconds * deviance));
+                }, Math.floor((1000 * outTransPercent * seconds) - deviance));
 
-            }, Math.floor(1000 * inT * seconds));
+            }, Math.floor(1000 * inTransPercent * seconds));
 
             timeout();
         }, seconds * 1000);
     }
-
     timeout();
 
   };
@@ -228,7 +172,7 @@
 
 let animatetext = function(){
   let $elements = $(".tt-holder .tt");
-  $(".ttt").writeText($elements, 16, 4, 20);
+  $(".ttt").writeText($elements, 17, 5, 30);
 }
 
 $(animatetext) ;
